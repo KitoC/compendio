78829c85512f22d930cb46a5ab81e05b4a6969de
Kito Clark <kito.clark@teamform.co>
2025-03-18 10:03:00 +1100

    fixed mobile side panel styling

--- src/components/ui/sidebar/sidebar.tsx
+++ src/components/ui/sidebar/sidebar.tsx
@@ -46,10 +46,10 @@ export const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
+            className="w-[--sidebar-width] bg-background p-0 text-sidebar-foreground [&>button]:hidden"
             style={
               {
-                "--sidebar-width": "18rem",
+                "--sidebar-width": "80%",
               } as React.CSSProperties
             }
             side={side}
